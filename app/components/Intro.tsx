7cbf068e0d529f8b40073c735462d05cf5d781b7
bhanuprakash026 <bhanutankasala26@gmail.com>
2025-02-08 23:53:10 +0530

    Added Resume

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -80,7 +80,7 @@ const Intro = () => {
         </Link>
 
         <a className="bg-gradient-to-r from-purple-500 to-indigo-500 text-white px-6 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition shadow-lg"
-            href="/public/Bhanu_Prakash_Frontend_Developer_1Year_8Months_Resume.pdf" download>
+            href="/Bhanu_Prakash_Frontend_Developer_1Year_8Months_Resume.pdf" download>
             Download Resume <HiDownload />
         </a>
 
